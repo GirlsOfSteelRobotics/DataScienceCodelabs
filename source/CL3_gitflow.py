715972cdba704d29ab2e84034ef6b9335dd0e0c0
Xiangjin <xiangjin@Xiangjins-MacBook-Air-5.local>
2026-09-21 18:56:57 -0400

    jinjin's second code lab

--- source/CL3_gitflow.py
+++ source/CL3_gitflow.py
@@ -6,7 +6,10 @@ def pj_says_hello_2026():
 
 def main():
     pj_says_hello_2026()
+    jinjin_says_bye()
 
+def jinjin_says_bye():
+    print("bye")
 
 if __name__ == "__main__":
     # python3 source/CL3_gitflow.py
