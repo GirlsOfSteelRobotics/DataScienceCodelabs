3ff66f6992e1240b7127f251ca663928983d935b
Sophie Ma <sophiema@sophies-imac-2.wifi.local.cmu.edu>
2026-09-21 18:56:54 -0400

    Sophie's git codelab

--- source/CL3_gitflow.py
+++ source/CL3_gitflow.py
@@ -3,9 +3,13 @@
 def pj_says_hello_2026():
     print("PJ Says Hello World")
 
+def sophie_says_goodbye():
+    print("Sophie Says Goodbye PJ")
+
 
 def main():
     pj_says_hello_2026()
+    sophie_says_goodbye()
 
 
 if __name__ == "__main__":
