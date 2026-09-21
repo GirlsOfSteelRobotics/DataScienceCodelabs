f252adfb0d5279b0100e61e96ade56c86487775a
PJ Reiniger <pj.reiniger@gmail.com>
2026-09-21 19:08:34 -0400

    PJs second git codelab (#2)

--- source/CL3_gitflow.py
+++ source/CL3_gitflow.py
@@ -4,8 +4,13 @@ def pj_says_hello_2026():
     print("PJ Says Hello World")
 
 
+def pj_says_hello_again():
+    print("PJ says hello again")
+
+
 def main():
     pj_says_hello_2026()
+    pj_says_hello_again()
 
 
 if __name__ == "__main__":
